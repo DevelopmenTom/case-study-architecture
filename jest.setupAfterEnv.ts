ff41042d892e218e227868a363e4d5e19de027ba
Tom Givol <tom.givol@mieterengel.de>
2026-02-22 13:20:08 +0100

    test: get setupDataSource from initializeDataSource helper
    working towards just 1 dataSource at all times that is also
    teared down after all tests run

--- jest.setupAfterEnv.ts
+++ jest.setupAfterEnv.ts
@@ -1,16 +1,20 @@
-import { DataSource } from 'typeorm';
-
 import { User } from './src/entities';
+import { initializeDataSource } from './src/lib';
 import { mockUserData } from './src/testHelpers/mockUserData';
-import { getDataSource } from './src/typeormconfig';
-
-let setupDataSource: DataSource;
 
 // making sure the User table exists in a fresh DB (e.g in CI) to enable running tests in parallel
 beforeAll(async () => {
-    setupDataSource = await getDataSource();
-    await setupDataSource.initialize();
+    const setupDataSource = await initializeDataSource();
 
     const userRepository = setupDataSource.getRepository(User);
     await userRepository.save(mockUserData());
 }, 30000);
+
+
+afterAll(async () => {
+    const setupDataSource = await initializeDataSource();
+    
+    if (setupDataSource?.isInitialized) {
+        await setupDataSource.destroy();
+    }
+});
