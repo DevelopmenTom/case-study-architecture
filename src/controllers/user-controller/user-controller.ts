c322e9ac054502052f00fa07db3d839f7950f756
Tom Givol <tom.givol@mieterengel.de>
2026-02-22 05:05:46 +0100

    fix: first authenticate, then validate on PUT profile

--- src/controllers/user-controller/user-controller.ts
+++ src/controllers/user-controller/user-controller.ts
@@ -193,8 +193,8 @@ export class UserController extends BaseController {
     @httpPut(
         '/profile',
         rateLimitMiddleware(),
-        validateRequest(UpdateProfileDto),
-        authenticateRequest()
+        authenticateRequest(),
+        validateRequest(UpdateProfileDto)
     )
     async updateProfile(@request() req: Request, @response() res: Response) {
         const { userId, ...updateData } = req.body as UpdateProfileDto;
